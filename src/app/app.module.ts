444610c850ce4e2fa149af658bf4e4a6fcf39146
Jeffrey93 <ingenierojeffry@gmail.com>
2017-08-19 11:17:03 -0500

    Agregada configuración de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4_c2k0kIILMlHmOR2ZNVsXYxTLDQJtT4",
+  authDomain: "ateco-5e8b3.firebaseapp.com",
+  databaseURL: "https://ateco-5e8b3.firebaseio.com",
+  projectId: "ateco-5e8b3",
+  storageBucket: "ateco-5e8b3.appspot.com",
+  messagingSenderId: "998019993358"
+};
 @NgModule({
   declarations: [
     AppComponent
